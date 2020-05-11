2d5c5ebef8f135b58087c9427083e6a51085c320
Kit MacAllister <kmacallister@secondstory.com>
2020-05-11 10:31:01 -0700

    Initial project upload
    Fixed gitignore

---
+++ Assets/Scripts/CrateSpawner.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CrateSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject dropTarget;
+    public float dropDistance = 5f;
+    public TextMeshProUGUI scoreText;
+    public int highScore = 0;
+    public CinemachineTargetGroup cmtg;
+    
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            dropTarget.SetActive(true);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = getHitPoint();
+            dropTarget.transform.position = pos;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (dropTarget.transform.position.y > highScore)
+            {
+                highScore = Mathf.CeilToInt(dropTarget.transform.position.y);
+                scoreText.text = highScore.ToString();
+            }
+            DropCrate();
+            dropTarget.SetActive(false);
+        }
+        
+        // Explode crate
+        if (Input.GetMouseButtonUp(1))
+        {
+            GameObject go = getHitObject();
+            if (go.GetComponent<collisionDamage>() != null)
+            {
+                go.GetComponent<collisionDamage>().Explode();   
+            }
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (!Mathf.Approximately(Input.GetAxis("Horizontal"),0))
+        {
+            dropTarget.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Horizontal"));
+        }
+    }
+
+    private void DropCrate()
+    {
+        Vector3 pos = getHitPoint();
+        GameObject go = Instantiate(prefab, pos, dropTarget.transform.rotation, transform);
+        cmtg.AddMember(go.transform, 1f, 3f);
+    }
+
+    private Vector3 getHitPoint()
+    {
+        RaycastHit hit;
+        Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit);
+        return hit.point + Vector3.up * dropDistance;
+    }
+    
+    private GameObject getHitObject()
+    {
+        RaycastHit hit;
+        Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit);
+        return hit.collider.gameObject;
+    }
+}
---
+++ Assets/Scripts/collisionDamage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class collisionDamage : MonoBehaviour
+{
+    public float damage = 0f;
+    public float damageIntensity = 0.0001f;
+    public float explosionForce = 1000f;
+    public float explosionRadius = 1f;
+    public float upwardsModifier = 1f;
+    public CinemachineTargetGroup cmtg;
+    
+    public GameObject solid;
+    public GameObject exploded;
+    
+    private Material solidMat;
+    private Rigidbody solidRB;
+    private GameObject[] explodedChildren;
+    private Material[] explodedMat;
+    private Rigidbody[] explodedRB;
+
+    private int count;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = exploded.transform.childCount;
+        solidMat = solid.GetComponent<Renderer>().material;
+        solidRB = GetComponent<Rigidbody>();
+        explodedChildren = new GameObject[count];
+        explodedMat = new Material[count];
+        explodedRB = new Rigidbody[count];
+        for(int i = 0; i < count; i++)
+        {
+            explodedChildren[i] = exploded.transform.GetChild(i).gameObject;
+            explodedMat[i] = exploded.transform.GetChild(i).GetComponent<Renderer>().material;
+            explodedRB[i] = exploded.transform.GetChild(i).GetComponent<Rigidbody>();
+        }
+
+        cmtg = GameObject.FindGameObjectWithTag("TargetGroup").GetComponent<CinemachineTargetGroup>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        damage += collision.impulse.magnitude * damageIntensity;
+        Mathf.Clamp(damage, 0f, 1f);
+        solidMat.SetFloat("damageBlend", damage);
+        if (damage >= 1f)
+        {
+            Explode();
+        }
+    }
+
+    public void Explode()
+    {
+        exploded.transform.position = solid.transform.position;
+        exploded.transform.rotation = solid.transform.rotation;
+        solid.SetActive(false);
+        solidRB.detectCollisions = false;
+        exploded.SetActive(true);
+        cmtg.RemoveMember(transform);
+        for (int i = 0; i < count; i++)
+        {
+            explodedMat[i].SetFloat("damageBlend", damage);
+            explodedRB[i].AddExplosionForce(explosionForce, transform.position, explosionRadius, upwardsModifier);
+            StartCoroutine(FreezeCollisions(i,3f));
+        }
+    }
+
+    private IEnumerator FreezeCollisions(int i, float timeout)
+    {
+        yield return new WaitForSeconds(timeout);
+        explodedRB[i].detectCollisions = false;
+        yield return new WaitForSeconds(timeout);
+        Destroy(gameObject);
+    }
+}
